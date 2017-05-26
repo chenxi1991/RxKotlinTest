4dff680cf931304f7b8e73b603025bf2de45ff4e
陈熙 <390923180@qq.com>
2017-05-26 17:38:45 +0800

    hello kotlin

--- app/src/main/java/hellowKotlin/hellow.kt
+++ app/src/main/java/hellowKotlin/hellow.kt
@@ -5,8 +5,11 @@ package hellowKotlin
  */
 object KotlinMain {
     @JvmStatic fun main(attrs: Array<String>) {
-        println(sum1(1, 2))
-        printSum(2, 3)
+        println(sum(1, 2))
+        println(sum1(2, 3))
+        printSum(3, 4)
+        varAndval()
+        maxOf(4, 5)
     }
 
     fun sum(a: Int, b: Int): Int {
@@ -15,7 +18,18 @@ object KotlinMain {
 
     fun sum1(a: Int, b: Int) = a + b
 
-    fun printSum(a: Int, b: Int) {
-        print("sum of $a and $b is ${a + b}")
+    fun printSum(a: Int, b: Int): Unit {
+        println("sum of $a and $b is ${a + b}")
     }
+
+    fun varAndval() {
+        val a: Int = 1
+        val b = 2
+//        b+=1 语法错误b是常量
+        var c = 3
+        c += 1
+        print("$a  $b  $c")
+    }
+
+    fun maxOf(a: Int, b: Int) = if (a > b) a else b
 }
